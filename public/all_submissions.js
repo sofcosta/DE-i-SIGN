f8d544732e6c31802453be04138cf2ddf4e11af5
sofcosta <100684371+sofcosta@users.noreply.github.com>
2023-05-08 10:23:57 +0100

    voting added (vote one time only)

--- public/all_submissions.js
+++ public/all_submissions.js
@@ -3,16 +3,11 @@ function build_all_submissions_page(submissions) {
     let all_submissions_container = document.getElementById('all_submissions_container');
     let submission_containers = document.getElementsByClassName('single_sub');
     submission_containers.innerHTML = "";
-    //console.log(submissions);
+    all_submissions_container.innerHTML = "";
     for (sub of submissions) {
-        // console.log(sub);
-        //console.log(sub.submissionId);
         let submission_container = document.createElement('button');
         submission_container.className = "single_sub";
         submission_container.id = sub.submissionId;
-        // submission_container.onclick = function (sub){
-        //     open_single_submission(sub);
-        // };
 
         let sub_title = document.createElement('h3');
         sub_title.appendChild(document.createTextNode(sub.submissionTitle));
@@ -24,7 +19,7 @@ function build_all_submissions_page(submissions) {
         sub_media.src = sub.submissionMedia;
 
         submission_container.appendChild(sub_title);
-        submission_container.appendChild(sub_desc);
+        //submission_container.appendChild(sub_desc);
         submission_container.appendChild(sub_media);
 
         all_submissions_container.appendChild(submission_container);
@@ -40,8 +35,6 @@ function build_all_submissions_page(submissions) {
             function findSubById(sub) {
                 return sub.submissionId === id;
             }
-            // for (sub of submissions) {
-            //     if (sub.submissionId == id) {
             let submission_container = document.createElement('div');
             submission_container.className = "single_sub";
             submission_container.id = sub.submissionId;
@@ -61,15 +54,16 @@ function build_all_submissions_page(submissions) {
             vote_slider.max = '100';
 
             let vote_submit = document.createElement('button');
-            // vote_submit.onclick = function () {
-            //     console.log(thisSub.submissionScore);
-            //     firebase_app.firestore().collection('submissions').doc(id).update({
-            //         submissionScore: parseInt(thisSub.submissionScore) + parseInt(vote_slider.value)
-            //     }).then(function () {
-            //         console.log("Score updated");
-            //         console.log(vote_slider.value);
-            //     })
-            // }
+            vote_submit.appendChild(document.createTextNode('Vote'));
+            vote_submit.onclick = function () {
+                console.log(thisSub.submissionScore);
+                firebase_app.firestore().collection('submissions').doc(id).update({
+                    submissionScore: parseInt(vote_slider.value) //parseInt(thisSub.submissionScore) + 
+                }).then(function () {
+                    console.log("Score updated");
+                    console.log(vote_slider.value);
+                })
+            }
 
             submission_container.appendChild(sub_media);
             submission_container.appendChild(sub_title);
@@ -83,7 +77,5 @@ function build_all_submissions_page(submissions) {
             console.log(single_submission.getAttribute('id'));
             show_single_submission_page();
         };
-        //     }
-        // }
     }
 }
