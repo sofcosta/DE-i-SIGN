0277efc29c2ac2e7f8cd3dc6b925f6196fedad84
MargaridaSouto19 <meggysouto@gmail.com>
2023-05-17 22:54:58 +0100

    created feednplay screen

---
+++ public/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCg6AsLPct_qFGm1eUvBYLb-7qHC3t33rA",
+  authDomain: "csc-firebase-app.firebaseapp.com",
+  databaseURL: "https://csc-firebase-app-default-rtdb.firebaseio.com",
+  projectId: "csc-firebase-app",
+  storageBucket: "csc-firebase-app.appspot.com",
+  messagingSenderId: "911710783408",
+  appId: "1:911710783408:web:c0229d31550e0761bb7875",
+  measurementId: "G-YDGCPFB2Y1"
+};
+
+firebase.initializeApp(firebaseConfig);
